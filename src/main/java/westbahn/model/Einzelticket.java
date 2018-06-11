9888127d0e9b833c297fe4cb93e1e200704a67f3
ntesanovic-tgm <ntesanovic@student.tgm.ac.at>
2018-06-11 11:05:16 +0200

    mapped Einzelticket model

--- src/main/java/westbahn/model/Einzelticket.java
+++ src/main/java/westbahn/model/Einzelticket.java
@@ -1,10 +1,34 @@
 package westbahn.model;
 
+import westbahn.Zahlung;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 
+@Entity
 public class Einzelticket extends Ticket {
 
+	@Enumerated(
+		EnumType.STRING
+	)
+	@Column
 	private TicketOption ticketOption;
 
+	public Einzelticket() {}
+
+	public Einzelticket(Strecke strecke, Zahlung zahlung, TicketOption ticketOption) {
+		this.ticketOption = ticketOption;
+		this.strecke = strecke;
+		this.zahlung = zahlung;
+	}
+
+	public TicketOption getTicketOption() {
+		return ticketOption;
+	}
+
+	public void setTicketOption(TicketOption ticketOption) {
+		this.ticketOption = ticketOption;
+	}
 }
