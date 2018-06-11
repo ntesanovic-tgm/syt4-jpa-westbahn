e11b445ce2a708d0a1cc45bdb7628010d29c4a32
ntesanovic-tgm <ntesanovic@student.tgm.ac.at>
2018-06-11 11:20:06 +0200

    mapped Preisstaffelung model

--- src/main/java/westbahn/model/Preisstaffelung.java
+++ src/main/java/westbahn/model/Preisstaffelung.java
@@ -1,21 +1,27 @@
 package westbahn.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 public class Preisstaffelung {
 
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private static long serialVersionUID;
 
+	@Column
 	private float grossGepaeck = 1.02f;
 
+	@Column
 	private float fahrrad = 1.05f;
 
+	@Column
 	private int zeitkarteWoche = 8;
 
+	@Column
 	private int zeitkarteMonat = 25;
 
+	@Column
 	private int zeitkarteJahr = 250;
 
 	private static Preisstaffelung instance;
@@ -28,4 +34,63 @@ public class Preisstaffelung {
 
 	}
 
+	public Preisstaffelung(float grossGepaeck, float fahrrad, int zeitkarteWoche, int zeitkarteMonat, int zeitkarteJahr) {
+		this.grossGepaeck = grossGepaeck;
+		this.fahrrad = fahrrad;
+		this.zeitkarteWoche = zeitkarteWoche;
+		this.zeitkarteMonat = zeitkarteMonat;
+		this.zeitkarteJahr = zeitkarteJahr;
+	}
+
+	public static long getSerialVersionUID() {
+		return serialVersionUID;
+	}
+
+	public static void setSerialVersionUID(long serialVersionUID) {
+		Preisstaffelung.serialVersionUID = serialVersionUID;
+	}
+
+	public float getGrossGepaeck() {
+		return grossGepaeck;
+	}
+
+	public void setGrossGepaeck(float grossGepaeck) {
+		this.grossGepaeck = grossGepaeck;
+	}
+
+	public float getFahrrad() {
+		return fahrrad;
+	}
+
+	public void setFahrrad(float fahrrad) {
+		this.fahrrad = fahrrad;
+	}
+
+	public int getZeitkarteWoche() {
+		return zeitkarteWoche;
+	}
+
+	public void setZeitkarteWoche(int zeitkarteWoche) {
+		this.zeitkarteWoche = zeitkarteWoche;
+	}
+
+	public int getZeitkarteMonat() {
+		return zeitkarteMonat;
+	}
+
+	public void setZeitkarteMonat(int zeitkarteMonat) {
+		this.zeitkarteMonat = zeitkarteMonat;
+	}
+
+	public int getZeitkarteJahr() {
+		return zeitkarteJahr;
+	}
+
+	public void setZeitkarteJahr(int zeitkarteJahr) {
+		this.zeitkarteJahr = zeitkarteJahr;
+	}
+
+	public static void setInstance(Preisstaffelung instance) {
+		Preisstaffelung.instance = instance;
+	}
 }
