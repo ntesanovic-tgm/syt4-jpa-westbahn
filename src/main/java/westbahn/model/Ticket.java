4143d502246d2673228285c70b0b8b55c980d73a
ntesanovic-tgm <ntesanovic@student.tgm.ac.at>
2018-06-11 11:04:17 +0200

    mapped Ticket model

--- src/main/java/westbahn/model/Ticket.java
+++ src/main/java/westbahn/model/Ticket.java
@@ -2,17 +2,44 @@ package westbahn.model;
 
 import westbahn.Zahlung;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
+@Entity
 public abstract class Ticket {
 
 	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long ID;
 
+	@ManyToOne
+	@JoinColumn
 	protected Strecke strecke;
 
+	@Transient
 	protected Zahlung zahlung;
 
+	public long getID() {
+		return ID;
+	}
+
+	public void setID(long ID) {
+		this.ID = ID;
+	}
+
+	public Strecke getStrecke() {
+		return strecke;
+	}
+
+	public void setStrecke(Strecke strecke) {
+		this.strecke = strecke;
+	}
+
+	public Zahlung getZahlung() {
+		return zahlung;
+	}
+
+	public void setZahlung(Zahlung zahlung) {
+		this.zahlung = zahlung;
+	}
 }
