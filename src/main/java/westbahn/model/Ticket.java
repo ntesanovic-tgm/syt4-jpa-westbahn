484cb4c1bc834e45aa2ec8765a75833801bb43d3
ntesanovic-tgm <ntesanovic@student.tgm.ac.at>
2018-06-14 00:02:58 +0200

    Changed the Ticket query

--- src/main/java/westbahn/model/Ticket.java
+++ src/main/java/westbahn/model/Ticket.java
@@ -4,11 +4,15 @@ import westbahn.Zahlung;
 
 import javax.persistence.*;
 
+@NamedQuery(
+		name = "Ticket.TicketsWithoutReservation",
+		query = "SELECT t FROM Benutzer b JOIN b.tickets t WHERE t.strecke.start=:start AND t.strecke.ende=:ende AND t.strecke NOT in (SELECT r.strecke FROM b.reservierungen r)"
+		)
+
 @Entity
 public abstract class Ticket {
 
 	@Id
-	@Column
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long ID;
 
